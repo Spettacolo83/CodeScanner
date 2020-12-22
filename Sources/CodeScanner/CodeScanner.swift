8877256dd940529bb1fc90f257a712a57261463b
xremix <tonidhoffmann@gmail.com>
2020-12-22 19:51:05 +0100

    zoom and scan result

--- Sources/CodeScanner/CodeScanner.swift
+++ Sources/CodeScanner/CodeScanner.swift
@@ -12,6 +12,7 @@ import SwiftUI
 /// To use, set `codeTypes` to be an array of things to scan for, e.g. `[.qr]`, and set `completion` to
 /// a closure that will be called when scanning has finished. This will be sent the string that was detected or a `ScanError`.
 /// For testing inside the simulator, set the `simulatedData` property to some test data you want to send back.
+
 public struct CodeScannerView: UIViewControllerRepresentable {
     public enum ScanError: Error {
         case badInput, badOutput
@@ -20,57 +21,65 @@ public struct CodeScannerView: UIViewControllerRepresentable {
     public enum ScanMode {
         case once, oncePerCode, continuous
     }
-
+    
+    public struct ScanResult{
+        var rawBarcode: String?
+        var error: ScanError?
+        var type: AVMetadataObject.ObjectType?
+    }
+    
     public class ScannerCoordinator: NSObject, AVCaptureMetadataOutputObjectsDelegate {
         var parent: CodeScannerView
         var codesFound: Set<String>
         var isFinishScanning = false
         var lastTime = Date(timeIntervalSince1970: 0)
-
+        
         init(parent: CodeScannerView) {
             self.parent = parent
             self.codesFound = Set<String>()
         }
-
+        
         public func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
             if let metadataObject = metadataObjects.first {
                 guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
                 guard let stringValue = readableObject.stringValue else { return }
                 guard isFinishScanning == false else { return }
-
+                
                 switch self.parent.scanMode {
                 case .once:
-                    found(code: stringValue)
+                    found(result: ScanResult(rawBarcode: stringValue, error: nil, type: readableObject.type))
                     // make sure we only trigger scan once per use
                     isFinishScanning = true
                 case .oncePerCode:
                     if !codesFound.contains(stringValue) {
                         codesFound.insert(stringValue)
-                        found(code: stringValue)
+                        found(result: ScanResult(rawBarcode: stringValue, error: nil, type: readableObject.type))
                     }
                 case .continuous:
                     if isPastScanInterval() {
-                        found(code: stringValue)
+                        found(result: ScanResult(rawBarcode: stringValue, error: nil, type: readableObject.type))
                     }
                 }
             }
         }
-
+        
         func isPastScanInterval() -> Bool {
             return Date().timeIntervalSince(lastTime) >= self.parent.scanInterval
         }
         
-        func found(code: String) {
+        func found(result: ScanResult) {
             lastTime = Date()
-            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
-            parent.completion(.success(code))
+            //            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            //            parent.completion(.success(result))
+            parent.completion(result)
         }
-
+        
         func didFail(reason: ScanError) {
-            parent.completion(.failure(reason))
+            let result = ScanResult(rawBarcode: nil, error: reason, type: nil)
+            parent.completion(result)
         }
     }
-
+    
     #if targetEnvironment(simulator)
     public class ScannerViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
         var delegate: ScannerCoordinator?
@@ -80,7 +89,7 @@ public struct CodeScannerView: UIViewControllerRepresentable {
             let label = UILabel()
             label.translatesAutoresizingMaskIntoConstraints = false
             label.numberOfLines = 0
-
+            
             label.text = "You're running in the simulator, which means the camera isn't available. Tap anywhere to send back some simulated data."
             label.textAlignment = .center
             let button = UIButton()
@@ -89,16 +98,16 @@ public struct CodeScannerView: UIViewControllerRepresentable {
             button.setTitleColor(UIColor.systemBlue, for: .normal)
             button.setTitleColor(UIColor.gray, for: .highlighted)
             button.addTarget(self, action: #selector(self.openGallery), for: .touchUpInside)
-
+            
             let stackView = UIStackView()
             stackView.translatesAutoresizingMaskIntoConstraints = false
             stackView.axis = .vertical
             stackView.spacing = 50
             stackView.addArrangedSubview(label)
             stackView.addArrangedSubview(button)
-
+            
             view.addSubview(stackView)
-
+            
             NSLayoutConstraint.activate([
                 button.heightAnchor.constraint(equalToConstant: 50),
                 stackView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor),
@@ -106,37 +115,38 @@ public struct CodeScannerView: UIViewControllerRepresentable {
                 stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
             ])
         }
-
+        
         override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
             guard let simulatedData = delegate?.parent.simulatedData else {
                 print("Simulated Data Not Provided!")
                 return
             }
-
-            delegate?.found(code: simulatedData)
+            
+            delegate?.found(result: ScanResult(rawBarcode: simulatedData))
         }
-
+        
         @objc func openGallery(_ sender: UIButton){
             let imagePicker = UIImagePickerController()
             imagePicker.delegate = self
             self.present(imagePicker, animated: true, completion: nil)
         }
-
+        
         public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
             if let qrcodeImg = info[.originalImage] as? UIImage {
                 let detector:CIDetector=CIDetector(ofType: CIDetectorTypeQRCode, context: nil, options: [CIDetectorAccuracy:CIDetectorAccuracyHigh])!
                 let ciImage:CIImage=CIImage(image:qrcodeImg)!
                 var qrCodeLink=""
-
+                
                 let features=detector.features(in: ciImage)
                 for feature in features as! [CIQRCodeFeature] {
                     qrCodeLink += feature.messageString!
                 }
-
+                
                 if qrCodeLink=="" {
                     delegate?.didFail(reason: .badOutput)
                 }else{
-                    delegate?.found(code: qrCodeLink)
+                    //                    delegate?.found(code: qrCodeLink)
+                    delegate?.found(result: CodeScannerView.ScanResult(rawBarcode: qrCodeLink))
                 }
             }
             else{
@@ -149,41 +159,51 @@ public struct CodeScannerView: UIViewControllerRepresentable {
     public class ScannerViewController: UIViewController {
         var captureSession: AVCaptureSession!
         var previewLayer: AVCaptureVideoPreviewLayer!
+        private var videoCaptureDevice: AVCaptureDevice? = AVCaptureDevice.default(for: .video)
         var delegate: ScannerCoordinator?
-
+        
+        public func zoom(level: CGFloat){
+            do{
+                try videoCaptureDevice?.lockForConfiguration()
+                
+                videoCaptureDevice?.videoZoomFactor = level
+                videoCaptureDevice?.unlockForConfiguration()
+            }catch{}
+        }
+        
         override public func viewDidLoad() {
             super.viewDidLoad()
-
-
+            
             NotificationCenter.default.addObserver(self,
                                                    selector: #selector(updateOrientation),
                                                    name: Notification.Name("UIDeviceOrientationDidChangeNotification"),
                                                    object: nil)
-
+            
             view.backgroundColor = UIColor.black
             captureSession = AVCaptureSession()
-
-            guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+            
+//            /*guard let */ self.videoCaptureDevice = AVCaptureDevice.default(for: .video) //else { return }
+            //            videoCaptureDevice?.activeFormat = (videoCaptureDevice?.formats.last)!
             let videoInput: AVCaptureDeviceInput
-
+            
             do {
-                videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+                videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice!)
             } catch {
                 return
             }
-
+            
             if (captureSession.canAddInput(videoInput)) {
                 captureSession.addInput(videoInput)
             } else {
                 delegate?.didFail(reason: .badInput)
                 return
             }
-
+            
             let metadataOutput = AVCaptureMetadataOutput()
-
+            
             if (captureSession.canAddOutput(metadataOutput)) {
                 captureSession.addOutput(metadataOutput)
-
+                
                 metadataOutput.setMetadataObjectsDelegate(delegate, queue: DispatchQueue.main)
                 metadataOutput.metadataObjectTypes = delegate?.parent.codeTypes
             } else {
@@ -191,22 +211,22 @@ public struct CodeScannerView: UIViewControllerRepresentable {
                 return
             }
         }
-
+        
         override public func viewWillLayoutSubviews() {
             previewLayer?.frame = view.layer.bounds
         }
-
+        
         @objc func updateOrientation() {
             guard let orientation = UIApplication.shared.windows.first?.windowScene?.interfaceOrientation else { return }
             guard let connection = captureSession.connections.last, connection.isVideoOrientationSupported else { return }
             connection.videoOrientation = AVCaptureVideoOrientation(rawValue: orientation.rawValue) ?? .portrait
         }
-
+        
         override public func viewDidAppear(_ animated: Bool) {
             super.viewDidAppear(animated)
             updateOrientation()
         }
-
+        
         override public func viewWillAppear(_ animated: Bool) {
             super.viewWillAppear(animated)
             
@@ -216,58 +236,71 @@ public struct CodeScannerView: UIViewControllerRepresentable {
             previewLayer.frame = view.layer.bounds
             previewLayer.videoGravity = .resizeAspectFill
             view.layer.addSublayer(previewLayer)
-
+            
             if (captureSession?.isRunning == false) {
                 captureSession.startRunning()
             }
         }
-
+        
         override public func viewDidDisappear(_ animated: Bool) {
             super.viewDidDisappear(animated)
-
+            
             if (captureSession?.isRunning == true) {
                 captureSession.stopRunning()
             }
-
+            
             NotificationCenter.default.removeObserver(self)
         }
-
+        
         override public var prefersStatusBarHidden: Bool {
             return true
         }
-
+        
         override public var supportedInterfaceOrientations: UIInterfaceOrientationMask {
             return .all
         }
     }
     #endif
-
+    
     public let codeTypes: [AVMetadataObject.ObjectType]
     public let scanMode: ScanMode
     public let scanInterval: Double
+    private var _zoomLevel: Double?
+    
+    private let viewController = ScannerViewController()
+    
+    public var zoomLevel: Double?{
+        get{
+            return _zoomLevel
+        }
+        set{
+            viewController.zoom(level: CGFloat(newValue ?? 1))
+            _zoomLevel = newValue
+        }
+    }
     public var simulatedData = ""
-    public var completion: (Result<String, ScanError>) -> Void
-
-    public init(codeTypes: [AVMetadataObject.ObjectType], scanMode: ScanMode = .once, scanInterval: Double = 2.0, simulatedData: String = "", completion: @escaping (Result<String, ScanError>) -> Void) {
+    public var completion: (ScanResult) -> Void
+    
+    public init(codeTypes: [AVMetadataObject.ObjectType], scanMode: ScanMode = .once, scanInterval: Double = 2.0, simulatedData: String = "", completion: @escaping (ScanResult) -> Void) {
         self.codeTypes = codeTypes
         self.scanMode = scanMode
         self.scanInterval = scanInterval
         self.simulatedData = simulatedData
         self.completion = completion
     }
-
+    
     public func makeCoordinator() -> ScannerCoordinator {
         return ScannerCoordinator(parent: self)
     }
-
+    
     public func makeUIViewController(context: Context) -> ScannerViewController {
-        let viewController = ScannerViewController()
+        //        let viewController = ScannerViewController()
         viewController.delegate = context.coordinator
         return viewController
     }
-
+    
     public func updateUIViewController(_ uiViewController: ScannerViewController, context: Context) {
-
+        
     }
 }
 
